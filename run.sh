5367831a9883fcbd91875ff98a8d05e7848e6c8a
bluezebra-io <69618711+bluezebra-io@users.noreply.github.com>
2023-10-15 14:30:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,15 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 # clone Odoo directory
-git clone --depth=1 https://github.com/BlueZebra-Works-nl/odoo-16-docker-compose-mac.git $DESTINATION
+git clone --depth=1 https://github.com/minhng92/odoo-16-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
 sudo chmod -R 777 $DESTINATION
+# Load kernel extension caches (if necessary)
+sudo kmutil load -n -d $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
-sudo launchctl kickstart -k system/com.apple.kext.caches
 sed -i 's#10016#'$PORT'#g' $DESTINATION/docker-compose.yml
 sed -i 's#20016#'$CHAT'#g' $DESTINATION/docker-compose.yml
 # run Odoo
